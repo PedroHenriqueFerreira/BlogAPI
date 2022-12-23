598aaf80ee2861e99bedb356acae2679d8f8b8f7
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2022-12-23 12:36:16 -0300

    update

---
+++ config/api.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rest: {
+    defaultLimit: 25,
+    maxLimit: 100,
+    withCount: true,
+  },
+  options: {
+    draftAndPublish: true,
+    populateCreatorFields: true
+  },
+};
